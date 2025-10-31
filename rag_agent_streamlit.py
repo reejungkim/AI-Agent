f4b6be0d5eed3ce707684dbe0816cacb7af831bd
reejung.kim <reejung.kim@gmail.com>
2025-10-31 23:20:28 +0900

    Replaced deprecated version of importing text splitter with newer version with langchain

--- rag_agent_streamlit.py
+++ rag_agent_streamlit.py
@@ -4,7 +4,7 @@ import tempfile
 import time
 from typing import List
 from langchain_community.document_loaders import PyPDFLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import FAISS
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_groq import ChatGroq
